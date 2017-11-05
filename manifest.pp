7e8d7c43a2c0482ea017e00b779b251532d48d94
Jesse Luehrs <doy@tozt.net>
2017-11-05 02:17:15 -0500

    refactor

--- manifest.pp
+++ manifest.pp
@@ -1,5 +1,1 @@
-include tozt::users
-include tozt::bootstrap
-include tozt::mail
-include tozt::site
-include tozt::other_packages
+include tozt
---
+++ modules/tozt/manifests/init.pp
@@ -0,0 +1,7 @@
+class tozt {
+  include tozt::users
+  include tozt::bootstrap
+  include tozt::mail
+  include tozt::site
+  include tozt::other_packages
+}
