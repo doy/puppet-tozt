9bca57901671fb447ea0921441661bc49e4aa2e0
Jesse Luehrs <doy@tozt.net>
2017-11-05 01:41:04 -0500

    fix file path

--- modules/tozt/manifests/bootstrap.pp
+++ modules/tozt/manifests/bootstrap.pp
@@ -1,6 +1,6 @@
 class tozt::bootstrap {
   file { '/usr/local/bin/tozt-puppet':
-    source => 'puppet:///modules/bootstrap/tozt-puppet',
+    source => 'puppet:///modules/tozt/tozt-puppet',
     mode => '0755';
   }
 }
