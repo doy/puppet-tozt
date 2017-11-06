1fbbab13ff6c55e00e748ef519da34e7e3df666a
Jesse Luehrs <doy@tozt.net>
2017-11-05 22:15:05 -0500

    set up dependencies a bit better
    so that nginx will get restarted when config changes

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -5,5 +5,9 @@ class nginx {
   include nginx::config
   include nginx::service
 
-  Class['nginx::install'] -> Class['nginx::config'] -> Class['nginx::service']
+  Class['nginx::install'] -> Class['nginx::config']
+
+  Class['nginx::config'] ~> Class['nginx::service']
+  Class['nginx::install'] ~> Class['nginx::service']
+  Nginx::Site<| |> ~> Class['nginx::service']
 }
