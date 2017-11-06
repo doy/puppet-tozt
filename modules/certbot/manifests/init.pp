d89b535482b54490b19579b1e4916322d28329a4
Jesse Luehrs <doy@tozt.net>
2017-11-06 02:49:50 -0500

    add some cron scripts

--- modules/certbot/manifests/init.pp
+++ modules/certbot/manifests/init.pp
@@ -2,4 +2,10 @@ class certbot {
   package { 'certbot':
     ensure => installed;
   }
+
+  file { '/etc/cron.daily/certbot':
+    source => 'puppet:///modules/certbot/certbot',
+    mode => '0755',
+    require => Package['certbot'];
+  }
 }
--- modules/spamassassin/manifests/init.pp
+++ modules/spamassassin/manifests/init.pp
@@ -3,7 +3,12 @@ class spamassassin {
     ensure => installed;
   }
 
-  file { '/etc/mail/spamassassin/local.cf':
-    source => 'puppet:///modules/spamassassin/local.cf';
+  file {
+    '/etc/mail/spamassassin/local.cf':
+      source => 'puppet:///modules/spamassassin/local.cf';
+    '/etc/cron.daily/spamassassin':
+      source => 'puppet:///modules/spamassassin/spamassassin',
+      mode => '0755',
+      require => Package['spamassassin'];
   }
 }
