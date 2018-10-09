41d81e79d9ee31ead91387d973e102bb195060b1
Jesse Luehrs <doy@tozt.net>
2018-10-09 04:38:46 -0400

    configure wireguard

--- modules/wireguard/manifests/init.pp
+++ modules/wireguard/manifests/init.pp
@@ -2,4 +2,17 @@ class wireguard {
   package { ["linux-headers", "wireguard-tools"]:
     ensure => installed,
   }
+
+  secret { "/etc/wireguard/algo.conf":
+    source => "wireguard",
+  }
+
+  service { "wg-quick@algo":
+    ensure => running,
+    enable => true,
+    require => [
+      Package["wireguard-tools"],
+      Secret["/etc/wireguard/algo.conf"],
+    ],
+  }
 }
