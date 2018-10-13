d34606126d57472ed1c3ee458c25603eb4cf9564
Jesse Luehrs <doy@tozt.net>
2018-10-13 13:55:35 -0400

    fix path

--- modules/tozt/manifests/site.pp
+++ modules/tozt/manifests/site.pp
@@ -7,7 +7,7 @@ class tozt::site {
   }
 
   exec { "clone tozt.net":
-    command => "git clone git://github.com/doy/tozt-hugo",
+    command => "/usr/bin/git clone git://github.com/doy/tozt-hugo",
     user => "doy",
     cwd => "/home/doy/coding",
     creates => "/home/doy/coding/tozt-hugo",
