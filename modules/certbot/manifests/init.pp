fc7014a8ffea45ef85a50f2dedf429393f6a643e
Jesse Luehrs <doy@tozt.net>
2018-10-14 16:03:48 -0400

    populate the correct config directory

--- modules/certbot/manifests/init.pp
+++ modules/certbot/manifests/init.pp
@@ -1,8 +1,10 @@
 class certbot($config_dir=undef) {
   if $config_dir {
+    $_config_dir = $config_dir
     $config_dir_opts = " --config-dir ${config_dir}"
   }
   else {
+    $_config_dir = "/etc/letsencrypt"
     $config_dir_opts = ""
   }
 
@@ -25,21 +27,21 @@ class certbot($config_dir=undef) {
         Package['certbot'],
         Class['cron'],
       ];
-    '/etc/letsencrypt/renewal-hooks':
+    "${_config_dir}/renewal-hooks":
       ensure => directory,
       require => Package['certbot'];
-    '/etc/letsencrypt/renewal-hooks/deploy':
+    "${_config_dir}/renewal-hooks/deploy":
       ensure => directory,
-      require => File['/etc/letsencrypt/renewal-hooks'];
-    '/etc/letsencrypt/renewal-hooks/deploy/reload-cert':
+      require => File["${_config_dir}/renewal-hooks"];
+    "${_config_dir}/renewal-hooks/deploy/reload-cert":
       source => 'puppet:///modules/certbot/reload-cert',
-      require => File['/etc/letsencrypt/renewal-hooks/deploy'];
+      require => File["${_config_dir}/renewal-hooks/deploy"];
   }
 
   exec { "initial certbot run":
     # XXX update to real domain name
     command => "/usr/bin/certbot -n --agree-tos -m doy@tozt.net --nginx -d new.tozt.net${config_dir_opts}",
-    creates => "/etc/letsencrypt/live",
+    creates => "${_config_dir}/live",
     require => [
       Package["certbot"],
       # not Class["nginx"], because of circular dependencies with nginx::site
