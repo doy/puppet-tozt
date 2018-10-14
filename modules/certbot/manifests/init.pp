e3d4e2e7bf93356fafaff2398cec60d65d6b3873
Jesse Luehrs <doy@tozt.net>
2018-10-14 16:26:34 -0400

    try to fix initial certbot provisioning

--- modules/certbot/manifests/init.pp
+++ modules/certbot/manifests/init.pp
@@ -1,11 +1,9 @@
 class certbot($config_dir=undef) {
   if $config_dir {
     $_config_dir = $config_dir
-    $config_dir_opts = " --config-dir ${config_dir}"
   }
   else {
     $_config_dir = "/etc/letsencrypt"
-    $config_dir_opts = ""
   }
 
   include cron
@@ -36,17 +34,21 @@ class certbot($config_dir=undef) {
     "${_config_dir}/renewal-hooks/deploy/reload-cert":
       source => 'puppet:///modules/certbot/reload-cert',
       require => File["${_config_dir}/renewal-hooks/deploy"];
+    "/usr/local/bin/bootstrap-certbot":
+      source => 'puppet:///modules/certbot/bootstrap-certbot',
+      mode => '0755';
   }
 
   exec { "initial certbot run":
-    # XXX update to real domain name
-    command => "/usr/bin/certbot -n --agree-tos -m doy@tozt.net --nginx -d new.tozt.net${config_dir_opts}",
+    provider => shell,
+    command => "/usr/local/bin/bootstrap-certbot ${config_dir}",
     creates => "${_config_dir}/live",
     require => [
       Package["certbot"],
       # not Class["nginx"], because of circular dependencies with nginx::site
       Package["nginx"],
       Package["certbot-nginx"],
+      File['/usr/local/bin/bootstrap-certbot'],
     ],
   }
 }
