3f586ac7646d17ec58d77a0c111f75a2804fe856
Jesse Luehrs <doy@tozt.net>
2018-10-17 04:55:07 -0400

    make ssh keys owned by the correct user

--- modules/ssh/manifests/user.pp
+++ modules/ssh/manifests/user.pp
@@ -1,4 +1,4 @@
-define ssh::user($user=$name, $home=undef) {
+define ssh::user($user=$name, $group=$user, $home=undef) {
   $_home = $home ? {
     undef => $user ? {
       'root' => '/root',
@@ -9,8 +9,14 @@ define ssh::user($user=$name, $home=undef) {
 
   secret { "${_home}/.ssh/id_rsa":
     source => "ssh/${user}/privkey",
+    owner => $user,
+    group => $group,
+    mode => '0600',
   }
   secret { "${_home}/.ssh/id_rsa.pub":
     source => "ssh/${user}/pubkey",
+    owner => $user,
+    group => $group,
+    mode => '0600',
   }
 }
