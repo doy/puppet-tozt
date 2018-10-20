1134589f5361b35a562ed6927645669aacd75ebe
Jesse Luehrs <doy@tozt.net>
2018-10-20 01:28:04 -0400

    start of mailserver config

--- mail/mail/manifests/mailserver.pp
+++ mail/mail/manifests/mailserver.pp
@@ -1,3 +1,21 @@
 class mail::mailserver {
-  include docker
+  include mailserver
+
+  file {
+    "/usr/local/share/mailserver/docker-compose.yml":
+      ensure => link,
+      target => "/usr/local/share/mailserver/git/docker-compose.yml.dist",
+      require => Class["mailserver"];
+    "/usr/local/share/mailserver/.env":
+      source => "puppet:///modules/mail/mailserver-env",
+      require => Class["mailserver"];
+  }
+
+  service { "mailserver":
+    ensure => running,
+    require => [
+      Class["mailserver"],
+      Exec["systemctl daemon-reload"],
+    ];
+  }
 }
---
+++ mail/mailserver/manifests/init.pp
@@ -0,0 +1,33 @@
+class mailserver {
+  include docker
+  include systemd
+
+  exec { "install mailserver docker image":
+    command => "docker pull tvial/docker-mailserver:latest",
+    require => Class["docker"];
+  }
+
+  file { "/usr/local/share/mailserver":
+    ensure => directory;
+  }
+
+  exec { "install mailserver repository":
+    command => "git clone git://github.com/tomav/docker-mailserver /usr/local/share/mailserver/git",
+    require => File["/usr/local/share/mailserver"];
+  }
+
+  file {
+    "/usr/local/bin/mailserver-setup":
+      ensure => link,
+      target => "/usr/local/share/mailserver/git/setup.sh",
+      require => Exec["install mailserver repository"];
+    "/etc/systemd/system/mailserver.service":
+      source => "puppet:///modules/mailserver/service",
+      notify => Exec["systemctl daemon-reload"],
+      require => [
+        Class["docker"],
+        File["/usr/local/share/mailserver/docker-compose.yml"],
+        File["/usr/local/share/mailserver/.env"],
+      ];
+  }
+}
