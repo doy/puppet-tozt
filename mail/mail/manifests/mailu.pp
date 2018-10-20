3461e75ffd2b789521f71906e34fab385a5c691b
Jesse Luehrs <doy@tozt.net>
2018-10-20 14:43:21 -0400

    fix up dependencies

--- mail/mail/manifests/mailu.pp
+++ mail/mail/manifests/mailu.pp
@@ -23,12 +23,8 @@ class mail::mailu {
       cat /mailu/.env.tmpl /mailu/secret-key > /mailu/.env
       echo BIND_ADDRESS4=`curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/anchor_ipv4/address` >> /mailu/.env
     ",
-    creates => "/mailu/.env",
+    refreshonly => true,
     subscribe => [
-      Secret["/mailu/secret-key"],
-      File["/mailu/.env.tmpl"],
-    ],
-    require => [
       Secret["/mailu/secret-key"],
       File["/mailu/.env.tmpl"],
     ];
