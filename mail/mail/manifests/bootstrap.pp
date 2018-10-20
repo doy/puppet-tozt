12eb36ad6a9538a6e6c29ad127c7505ef3373b75
Jesse Luehrs <doy@tozt.net>
2018-10-19 23:38:48 -0400

    fix path

--- mail/mail/manifests/bootstrap.pp
+++ mail/mail/manifests/bootstrap.pp
@@ -8,7 +8,7 @@ class mail::bootstrap {
   }
 
   file { '/usr/local/bin/puppet-tozt':
-    source => 'puppet:///modules/tozt/puppet-tozt',
+    source => 'puppet:///modules/mail/puppet-tozt',
     mode => '0755';
   }
 }
