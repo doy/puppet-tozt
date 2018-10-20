8bb560d93c11fe20ed1c81a429c2579bccf8621c
Jesse Luehrs <doy@tozt.net>
2018-10-20 01:42:37 -0400

    create postfix user accounts

--- mail/mail/manifests/mailserver.pp
+++ mail/mail/manifests/mailserver.pp
@@ -9,6 +9,18 @@ class mail::mailserver {
     "/usr/local/share/mailserver/.env":
       source => "puppet:///modules/mail/mailserver-env",
       require => Class["mailserver"];
+    "/usr/local/share/mailserver/config":
+      ensure => directory,
+      require => Class["mailserver"];
+    # XXX regen this with real user/pass
+    "/usr/local/share/mailserver/config/postfix-accounts.cf":
+      source => "puppet:///modules/mail/postfix-accounts.cf",
+      require => Class["mailserver"];
+  }
+
+  exec { "create initial mailserver user":
+    command => "/usr/local/bin/mailserver-setup email add doy@newmail.tozt.net",
+    require => Class["mailserver"];
   }
 
   service { "mailserver":
@@ -16,6 +28,7 @@ class mail::mailserver {
     require => [
       Class["mailserver"],
       Exec["systemctl daemon-reload"],
+      File["/usr/local/share/mailserver/config/postfix-accounts.cf"],
     ];
   }
 }
