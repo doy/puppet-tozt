0be91408b40cb5951f56bc3b6f49b9c944078991
Jesse Luehrs <doy@tozt.net>
2018-10-20 14:37:20 -0400

    properly bind to the floating ip

--- mail/mail/manifests/mailu.pp
+++ mail/mail/manifests/mailu.pp
@@ -17,9 +17,12 @@ class mail::mailu {
     require => File["/mailu"];
   }
 
-  exec { "create env file with secret":
+  exec { "create env file":
     provider => shell,
-    command => "cat /mailu/.env.tmpl /mailu/secret-key > /mailu/.env",
+    command => "
+      cat /mailu/.env.tmpl /mailu/secret-key > /mailu/.env
+      echo BIND_ADDRESS4=`curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/anchor_ipv4/address` >> /mailu/.env
+    ",
     creates => "/mailu/.env",
     require => [
       Secret["/mailu/secret-key"],
