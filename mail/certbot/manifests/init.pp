b0c82f567367816683b01560dc68ebbc905611b3
Jesse Luehrs <doy@tozt.net>
2018-10-20 03:30:33 -0400

    can't use the nginx plugin
    the one that ships with debian is too old

--- mail/certbot/manifests/init.pp
+++ mail/certbot/manifests/init.pp
@@ -1,36 +1,37 @@
 class certbot {
-  package {
-    [
-      "certbot",
-      "nginx",
-      "python-certbot-nginx",
-    ]:
-      ensure => installed;
+  include nginx
+
+  package { "certbot":
+    ensure => installed;
   }
 
   file {
-    "/etc/nginx/sites-available/certbot":
-      source => "puppet:///modules/certbot/nginx",
-      require => Package["nginx"];
-    "/etc/nginx/sites-enabled/certbot":
-      ensure => link,
-      target => "../sites-available/certbot",
-      require => Package["nginx"];
     "/var/www/certbot":
       ensure => directory,
       require => Package["nginx"];
-    "/etc/nginx/sites-enabled/default":
-      ensure => absent,
+    "/etc/nginx/sites-available/certbot":
+      source => "puppet:///modules/certbot/nginx",
+      notify => Service["nginx"],
+      require => [
+        Package["nginx"],
+        File["/var/www/certbot"],
+      ];
+    "/etc/nginx/sites-enabled/certbot":
+      ensure => link,
+      target => "../sites-available/certbot",
+      notify => Service["nginx"],
       require => Package["nginx"];
   }
 
   exec { "initial certbot run":
-    command => "/usr/bin/certbot -n --agree-tos -m doy@tozt.net --nginx -d newmail.tozt.net",
+    command => "/usr/bin/certbot certonly -n --agree-tos -m doy@tozt.net --authenticator webroot --webroot-path /var/www/certbot -d newmail.tozt.net",
     creates => "/etc/letsencrypt/live",
     require => [
+      Class["nginx"],
       File["/etc/nginx/sites-enabled/certbot"],
+      File["/etc/nginx/sites-available/certbot"],
       File["/var/www/certbot"],
       Package["certbot"],
-    ],
+    ];
   }
 }
---
+++ mail/nginx/manifests/init.pp
@@ -0,0 +1,14 @@
+class nginx {
+  package { "nginx":
+    ensure => installed;
+  }
+
+  file { "/etc/nginx/sites-enabled/default":
+    ensure => absent,
+    require => Package["nginx"];
+  }
+
+  service { "nginx":
+    ensure => running;
+  }
+}
