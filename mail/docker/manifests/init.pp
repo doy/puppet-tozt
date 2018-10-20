6db0417dbe0cb10a0196e942f7bfb9ff290bbd83
Jesse Luehrs <doy@tozt.net>
2018-10-20 00:12:38 -0400

    update after installing repository and only install once

--- mail/docker/manifests/init.pp
+++ mail/docker/manifests/init.pp
@@ -15,7 +15,9 @@ class docker {
     command => "
       curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
       add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable\"
+      apt-get -yq update
     ",
+    unless => "grep -q download.docker.com /etc/apt/sources.list",
     require => [
       Package["apt-transport-https"],
       Package["ca-certificates"],
