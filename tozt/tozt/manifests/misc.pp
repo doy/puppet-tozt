94164f1e4c20adcaf80de580fc87ac14988d171e
Jesse Luehrs <doy@tozt.net>
2018-10-29 22:13:35 -0400

    generate /etc/locale.conf and /etc/hosts

---
+++ tozt/tozt/manifests/misc.pp
@@ -0,0 +1,20 @@
+class tozt::misc {
+  file {
+    "/etc/locale.gen":
+      content => "en_US.UTF-8 UTF-8\n",
+      notify => Exec["regen locale data"];
+    "/etc/locale.conf":
+      content => "LANG=en_US.UTF-8\n",
+      require => [
+        File["/etc/locale.gen"],
+        Exec["regen locale data"],
+      ];
+    "/etc/hosts":
+      source => "puppet:///modules/tozt/hosts";
+  }
+
+  exec { "regen locale data":
+    command => "/usr/bin/locale-gen",
+    refreshonly => true;
+  }
+}
