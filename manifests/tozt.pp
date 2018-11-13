7da364ee4b05870e85ef048709f53ae6b53fbe1a
Jesse Luehrs <doy@tozt.net>
2018-11-13 01:46:04 -0500

    set defaults even earlier

--- manifests/tozt.pp
+++ manifests/tozt.pp
@@ -2,18 +2,18 @@ node 'tozt.localdomain' {
   $default_user = 'doy'
   $persistent_data = '/media/persistent'
 
+  Package::Makepkg {
+    build_user => $default_user,
+  }
+
+  Base::User[$default_user] -> Package::Makepkg<| build_user == $default_user |>
+
   include tozt::persistent
   class { 'base':
     default_user => $default_user,
     persistent_data => $persistent_data,
   }
   Class['tozt::persistent'] -> Class['base']
-
-  Package::Makepkg {
-    build_user => $default_user,
-  }
-
-  Base::User[$default_user] -> Package::Makepkg<| build_user == $default_user |>
 
   include tozt::backups
   include tozt::git
