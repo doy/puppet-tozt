96470b3a7265f2fbac1947ba5baaff5fa9ed7f6d
Jesse Luehrs <doy@tozt.net>
2018-11-17 14:14:20 -0500

    restart samba when config files change

--- modules/partofme/manifests/filesharing.pp
+++ modules/partofme/manifests/filesharing.pp
@@ -4,7 +4,7 @@ class partofme::filesharing {
   file { '/etc/samba/smb.conf':
     source => 'puppet:///modules/partofme/smb.conf',
     require => Package['samba'],
-    before => [
+    notify => [
       Service['smb'],
       Service['nmb'],
     ];
