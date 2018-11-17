aa8d0286d54817259c95dcfe9634da3f7f03bc22
Jesse Luehrs <doy@tozt.net>
2018-11-17 03:25:43 -0500

    comments

--- manifests/partofme.pp
+++ manifests/partofme.pp
@@ -1,4 +1,7 @@
 node 'partofme.localdomain' {
+  # we don't want to store anything system-related on the persistent disk,
+  # since we want it to be encrypted, and we can't really access it at boot
+  # time to type in a password
   $persistent_data = undef
 
   include base
