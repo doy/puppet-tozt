415a3014a5d357c3f207e41a1bdb1ba44f140885
Jesse Luehrs <doy@tozt.net>
2018-11-18 02:29:45 -0500

    add duplicati user configuration

--- modules/partofme/manifests/backups.pp
+++ modules/partofme/manifests/backups.pp
@@ -3,4 +3,14 @@ class partofme::backups {
 
   syncthing::user { $default_user:
   }
+
+  user { 'duplicati':
+    home => '/media/persistent/duplicati',
+    password => secret::value('passwd/duplicati'),
+    require => Package::Makepkg['duplicati-latest'];
+  }
+
+  sshd::configsection { 'duplicati':
+    source => 'puppet:///modules/partofme/sshd_config.duplicati';
+  }
 }
