2a0b1d8dd5b26bbcbcd51b0218b80064df01ed8e
Jesse Luehrs <doy@tozt.net>
2018-11-18 05:10:46 -0500

    need to add duplicati's shell to /etc/shells

--- modules/partofme/manifests/backups.pp
+++ modules/partofme/manifests/backups.pp
@@ -4,13 +4,29 @@ class partofme::backups {
   syncthing::user { $default_user:
   }
 
+  file { '/usr/local/bin/sftp-only':
+    content => 'exec false',
+    mode => '0755';
+  }
+
   user { 'duplicati':
     home => '/media/persistent/duplicati',
     password => secret::value('passwd/duplicati'),
-    require => Package::Makepkg['duplicati-latest'];
+    shell => '/usr/local/bin/sftp-only',
+    require => [
+      Package::Makepkg['duplicati-latest'],
+      File['/usr/local/bin/sftp-only'],
+    ];
   }
 
   sshd::configsection { 'duplicati':
     source => 'puppet:///modules/partofme/sshd_config.duplicati';
+  }
+
+  exec { 'allow sftp logins for duplicati':
+    provider => 'shell',
+    command => 'echo /usr/local/bin/sftp-only >> /etc/shells',
+    unless => 'grep -qF /usr/local/bin/sftp-only /etc/shells',
+    require => File['/usr/local/bin/sftp-only'];
   }
 }
