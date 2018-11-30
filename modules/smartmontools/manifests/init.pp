8a7d39c3e63ba7e81a8eb2276b2e16b489fb08cc
Jesse Luehrs <doy@tozt.net>
2018-11-30 01:19:00 -0500

    restart smartd when smartd.conf is updated

--- modules/smartmontools/manifests/init.pp
+++ modules/smartmontools/manifests/init.pp
@@ -10,9 +10,7 @@ class smartmontools {
   service { 'smartd':
     ensure => running,
     enable => true,
-    require => [
-      Package['smartmontools'],
-      File['/etc/smartd.conf'],
-    ]
+    require => Package['smartmontools'],
+    subscribe => File['/etc/smartd.conf'];
   }
 }
