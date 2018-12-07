1986bb221595991ee07be78b951cfca81b6cee34
Jesse Luehrs <doy@tozt.net>
2018-12-07 00:37:07 -0500

    make sure the docker service is started

--- modules/docker/manifests/init.pp
+++ modules/docker/manifests/init.pp
@@ -2,4 +2,10 @@ class docker {
   package { "docker-compose":
     ensure => installed;
   }
+
+  service { "docker":
+    ensure => running,
+    enable => true,
+    require => Package["docker-compose"];
+  }
 }
