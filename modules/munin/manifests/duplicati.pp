c910094a7e16f7e64c8def606678c934cd44d0d1
Jesse Luehrs <doy@tozt.net>
2019-01-02 05:27:30 -0500

    typo

--- modules/munin/manifests/duplicati.pp
+++ modules/munin/manifests/duplicati.pp
@@ -1,28 +1,28 @@
 class munin::duplicati {
   file {
     "/usr/lib/munin/plugins/duplicati_":
-      content => "puppet:///modules/munin/duplicati_",
+      source => "puppet:///modules/munin/duplicati_",
       mode => "0755",
       require => Package["munin-node"],
       before => Service["munin-node"];
     "/usr/lib/munin/plugins/duplicati.rb":
-      content => "puppet:///modules/munin/duplicati.rb",
+      source => "puppet:///modules/munin/duplicati.rb",
       require => Package["munin-node"],
       before => Service["munin-node"];
     "/usr/lib/munin/plugins/duplicati_duration.rb":
-      content => "puppet:///modules/munin/duplicati_duration.rb",
+      source => "puppet:///modules/munin/duplicati_duration.rb",
       require => Package["munin-node"],
       before => Service["munin-node"];
     "/usr/lib/munin/plugins/duplicati_file_count.rb":
-      content => "puppet:///modules/munin/duplicati_file_count.rb",
+      source => "puppet:///modules/munin/duplicati_file_count.rb",
       require => Package["munin-node"],
       before => Service["munin-node"];
     "/usr/lib/munin/plugins/duplicati_file_size.rb":
-      content => "puppet:///modules/munin/duplicati_file_size.rb",
+      source => "puppet:///modules/munin/duplicati_file_size.rb",
       require => Package["munin-node"],
       before => Service["munin-node"];
     "/usr/lib/munin/plugins/duplicati_last_run.rb":
-      content => "puppet:///modules/munin/duplicati_last_run.rb",
+      source => "puppet:///modules/munin/duplicati_last_run.rb",
       require => Package["munin-node"],
       before => Service["munin-node"];
     "/usr/lib/munin/plugins/duplicati_duration":
