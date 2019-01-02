83e422435fadb68ec5648df7fda01e5517d4a34e
Jesse Luehrs <doy@tozt.net>
2019-01-01 19:57:40 -0500

    generate hosts data

--- manifests/00global.pp
+++ manifests/00global.pp
@@ -1,4 +1,11 @@
 $default_user = 'doy'
+$vpn_ips = {
+  'hush' => '10.19.49.3',
+  'partofme' => '10.19.49.4',
+  'phone' => '10.19.49.5',
+  'tozt' => '10.19.49.6',
+  'mail' => '10.19.49.7',
+}
 
 Package::Makepkg {
   build_user => $default_user,
