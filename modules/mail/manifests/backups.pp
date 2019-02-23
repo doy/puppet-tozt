d49c0e7479603b63ea2f786cf8d1eb4a9e231fda
Jesse Luehrs <doy@tozt.net>
2019-02-23 18:08:37 -0500

    add duplicati configuration

--- modules/mail/manifests/backups.pp
+++ modules/mail/manifests/backups.pp
@@ -2,4 +2,10 @@ class mail::backups {
   class { "tarsnap":
     source => "puppet:///modules/mail/acts.conf";
   }
+
+  $encrypt_passphrase = secret::value('duplicati-encrypt')
+  $url = secret::value('duplicati-url')
+  duplicati::backup { 'mail':
+    content => template('mail/duplicati-mail.json');
+  }
 }
