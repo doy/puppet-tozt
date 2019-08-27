bf12e588ff735e408783c396ae0cbeb48a7aa251
Jesse Luehrs <doy@tozt.net>
2019-08-27 00:39:03 -0400

    deps

--- modules/tozt/manifests/metabase.pp
+++ modules/tozt/manifests/metabase.pp
@@ -50,7 +50,7 @@ class tozt::metabase {
       mode => '0755',
       source => "puppet:///modules/tozt/metabase",
       require => [
-        Exec["install ynab-export"],
+        Package::Cargo["ynab-export for doy"],
         Exec["clone investments-sheet-export"],
         Secret["/home/doy/.config/ynab/api-key"],
         Secret["/home/doy/.config/google/investments-sheet"],
