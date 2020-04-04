1a1239d4675576228ef74bb6ae12d6c5de6412cf
Jesse Luehrs <doy@tozt.net>
2020-04-04 00:32:37 -0400

    add bitwarden nginx config

--- modules/tozt/manifests/bitwarden.pp
+++ modules/tozt/manifests/bitwarden.pp
@@ -1,7 +1,16 @@
 class tozt::bitwarden {
+  include tozt::certbot
   include tozt::persistent
 
   class { "bitwarden::server":
     data_dir => "/media/persistent/bitwarden";
+  }
+
+  nginx::site {
+    "bitwarden-tls":
+      source => 'puppet:///modules/tozt/nginx/bitwarden-tls.conf',
+      require => Class['certbot'];
+    "bitwarden":
+      source => 'puppet:///modules/tozt/nginx/bitwarden.conf';
   }
 }
