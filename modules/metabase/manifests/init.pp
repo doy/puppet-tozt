ec23c54b1db10f1d0d11ddca9c84bf0a97575892
Jesse Luehrs <doy@tozt.net>
2020-05-27 20:47:58 -0400

    bump metabase

--- modules/metabase/manifests/init.pp
+++ modules/metabase/manifests/init.pp
@@ -2,7 +2,7 @@ class metabase {
   include postgres
   include systemd
 
-  $metabase_version = "v0.35.1"
+  $metabase_version = "v0.35.3"
 
   package { "jre-openjdk-headless":
     ensure => installed;
