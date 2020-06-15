172caffbc19175c147d75bfb26a4c7972bcd248d
Jesse Luehrs <doy@tozt.net>
2020-06-15 03:48:03 -0400

    munin email alerts

--- modules/munin/manifests/init.pp
+++ modules/munin/manifests/init.pp
@@ -1,6 +1,9 @@
 class munin {
   include systemd
 
+  $from = "munin"
+  $password = secret::value('munin_email_password')
+
   package { 'munin':
   }
 
@@ -12,6 +15,12 @@ class munin {
       require => Package['munin'];
     '/etc/munin/munin.conf':
       source => 'puppet:///modules/munin/munin.conf',
+      require => Package['munin'];
+    '/etc/munin/msmtprc':
+      content => template('munin/msmtprc'),
+      owner => 'munin',
+      group => 'munin',
+      mode => '0600',
       require => Package['munin'];
   }
 
