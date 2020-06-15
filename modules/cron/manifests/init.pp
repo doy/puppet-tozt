2a4e73e06824a6d313c2e2b1282035b6e790620d
Jesse Luehrs <doy@tozt.net>
2020-06-15 02:51:02 -0400

    move to msmtp and send real cron emails

--- modules/cron/manifests/init.pp
+++ modules/cron/manifests/init.pp
@@ -1,21 +1,36 @@
 class cron {
-  package { ["cronie", "opensmtpd"]:
+  include systemd
+
+  $from = "${facts['networking']['hostname']}-cron"
+  $password = secret::value('cron_email_password')
+
+  package { ["cronie", "msmtp"]:
     ensure => installed,
+  }
+
+  file {
+    '/etc/msmtprc':
+      content => template('cron/msmtprc');
+    '/etc/aliases':
+      content => template('cron/aliases');
+    '/etc/systemd/system/cronie.service.d':
+      ensure => directory;
+    '/etc/systemd/system/cronie.service.d/override.conf':
+      source => 'puppet:///modules/cron/override.conf',
+      require => File['/etc/systemd/system/cronie.service.d'],
+      notify => [
+        Exec["/usr/bin/systemctl daemon-reload"],
+        Service['cronie'],
+      ];
   }
 
   service { 'cronie':
     ensure => running,
     enable => true,
-    require => Package['cronie'];
-  }
-
-  service { 'smtpd':
-    ensure => running,
-    enable => true,
-    require => Package['opensmtpd'];
-  }
-
-  file { '/etc/smtpd/smtpd.conf':
-    source => 'puppet:///modules/cron/smtpd.conf';
+    require => [
+      Package['cronie'],
+      File['/etc/systemd/system/cronie.service.d/override.conf'],
+      Exec["/usr/bin/systemctl daemon-reload"],
+    ];
   }
 }
