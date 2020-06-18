f5e2bdad0919f94063f85cf97f760e767c2f09b5
Jesse Luehrs <doy@tozt.net>
2020-06-18 01:09:58 -0400

    add gitea initialization scripts

--- modules/gitea/manifests/init.pp
+++ modules/gitea/manifests/init.pp
@@ -24,6 +24,16 @@ class gitea {
   $secret_key = secret::value('gitea_secret_key')
   $smtp_password = secret::value('gitea_smtp_password')
 
+  $gitea_username = secret::value('gitea_username')
+  $gitea_password = secret::value('gitea_password')
+  $gitea_email = secret::value('gitea_email')
+
+  $github_username = secret::value('github_username')
+  $github_password = secret::value('github_password')
+  $github_api_token = secret::value('github_api_token')
+  $github_oauth_key = secret::value('github_oauth_key')
+  $github_oauth_secret = secret::value('github_oauth_secret')
+
   file {
     '/media/persistent/gitea':
       ensure => directory,
@@ -56,5 +66,32 @@ class gitea {
       source => 'puppet:///modules/gitea/override.conf',
       notify => Exec["/usr/bin/systemctl daemon-reload"],
       require => File["/etc/systemd/system/gitea.service.d"];
+    '/usr/local/bin/github2gitea':
+      content => template('gitea/github2gitea'),
+      mode => "0755";
+    '/usr/local/bin/setup-gitea':
+      content => template('gitea/setup-gitea'),
+      mode => "0755";
+  }
+
+  exec { "initialize gitea":
+    provider => shell,
+    command => 'su -p gitea /usr/local/bin/setup-gitea && systemctl restart gitea && /usr/local/bin/github2gitea',
+    timeout => 3600,
+    environment => [
+      'USER=gitea',
+      'HOME=/media/persistent/gitea',
+      'GITEA_WORK_DIR=/media/persistent/gitea',
+    ],
+    onlyif => '
+      test ! -s /media/persistent/gitea/gitea.sqlite || \
+      test `sqlite3 /media/persistent/gitea/gitea.sqlite "select count(*) from user"` -eq 0
+    ',
+    require => [
+      File['/usr/local/bin/setup-gitea'],
+      File['/usr/local/bin/github2gitea'],
+      Package['gitea'],
+      Service['gitea'],
+    ]
   }
 }
