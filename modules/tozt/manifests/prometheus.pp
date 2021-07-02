95c8b30870c4e6cf9335880fc439066bb2373fb3
Jesse Luehrs <doy@tozt.net>
2021-07-01 22:36:09 -0400

    actually install the nginx config

--- modules/tozt/manifests/prometheus.pp
+++ modules/tozt/manifests/prometheus.pp
@@ -1,3 +1,11 @@
 class tozt::prometheus {
   include prometheus
+
+  nginx::site {
+    "prometheus-tls":
+      source => 'puppet:///modules/tozt/nginx/prometheus-tls.conf',
+      require => Class['certbot'];
+    "prometheus":
+      source => 'puppet:///modules/tozt/nginx/prometheus.conf';
+  }
 }
