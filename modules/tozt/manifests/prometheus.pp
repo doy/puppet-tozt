858abbdb938f089fa51db435a781ddb4127896ab
Jesse Luehrs <doy@tozt.net>
2021-07-01 22:58:16 -0400

    move prometheus tsdb to /media/persistent

--- modules/tozt/manifests/prometheus.pp
+++ modules/tozt/manifests/prometheus.pp
@@ -1,6 +1,20 @@
 class tozt::prometheus {
   include prometheus
 
+  file {
+    "/media/persistent/prometheus":
+      ensure => directory,
+      owner => "prometheus",
+      group => "prometheus",
+      require => [
+        File["/media/persistent"],
+        Package["prometheus"],
+      ];
+    "/etc/conf.d/prometheus":
+      source => "puppet:///modules/tozt/prometheus-service-conf",
+      require => Package["prometheus"];
+  }
+
   nginx::site {
     "prometheus-tls":
       source => 'puppet:///modules/tozt/nginx/prometheus-tls.conf',
