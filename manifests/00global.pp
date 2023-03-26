42008079e0b1b01ea72920ad8415f288a87fe8ae
Jesse Luehrs <doy@tozt.net>
2023-03-26 13:41:26 -0400

    default $persistent_data to undef

--- manifests/00global.pp
+++ manifests/00global.pp
@@ -1,6 +1,6 @@
 $default_user = lookup('default_user')
 $vpn_ips = lookup('vpn_ips')
-$persistent_data = lookup('persistent_data')
+$persistent_data = lookup('persistent_data', undef, undef, undef)
 
 Package::Makepkg {
   build_user => $default_user,
