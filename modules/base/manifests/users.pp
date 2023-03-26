2413b4060f5e124d442572ad52a6a9a74c70e0cd
Jesse Luehrs <doy@tozt.net>
2023-03-26 17:23:51 -0400

    don't use nested directories for secrets

--- modules/base/manifests/users.pp
+++ modules/base/manifests/users.pp
@@ -1,10 +1,10 @@
 class base::users {
   base::user { 'root':
-    pwhash => secret::value('passwd/root');
+    pwhash => secret::value('passwd_root');
   }
 
   base::user { $::default_user:
-    pwhash => secret::value("passwd/$default_user"),
+    pwhash => secret::value("passwd_$default_user"),
     extra_groups => ['wheel', 'video', 'audio'],
     homedir_mode => '0701';
   }
