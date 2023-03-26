ee0d0b20dfcbede3ec7d43023086443d6060dc55
Jesse Luehrs <doy@tozt.net>
2023-03-26 11:29:35 -0400

    also prune old containers

--- modules/podman/manifests/init.pp
+++ modules/podman/manifests/init.pp
@@ -2,4 +2,9 @@ class podman {
   package { ["podman", "cni-plugins"]:
     ensure => installed;
   }
+
+  cron::job { "podman-prune":
+    frequency => "daily",
+    source => 'puppet:///modules/podman/podman-prune';
+  }
 }
