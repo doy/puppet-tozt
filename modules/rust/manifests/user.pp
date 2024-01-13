c9dd1a739735ef6ec99aef0419aa84afccba2189
Jesse Luehrs <doy@tozt.net>
2024-01-13 15:44:43 -0500

    remove rust docs
    they are large, and unused on these machines

--- modules/rust/manifests/user.pp
+++ modules/rust/manifests/user.pp
@@ -12,4 +12,16 @@ define rust::user($user=$name) {
       User[$user],
     ],
   }
+
+  exec { "uninstall rust docs":
+    provider => "shell",
+    command => "rustup component remove rust-docs",
+    user => $user,
+    onlyif => "rustup component list | grep -q rust-docs",
+    require => [
+      Package["rustup"],
+      User[$user],
+      Exec["install and configure stable toolchain for $user"],
+    ],
+  }
 }
