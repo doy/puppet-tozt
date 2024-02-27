528974eb4b1b9316d2d6082771466c42b562c163
Jesse Luehrs <doy@tozt.net>
2024-02-26 21:29:10 -0500

    convert mlocate to use a timer instead of a cronjob

--- modules/locate/manifests/init.pp
+++ modules/locate/manifests/init.pp
@@ -7,17 +7,12 @@ class locate {
 
   cron::job { "updatedb":
     frequency => "daily",
-    source => 'puppet:///modules/locate/updatedb',
-    require => Package['mlocate'];
+    ensure => absent;
   }
 
-  exec { "initial updatedb run":
-    command => "/etc/cron.daily/updatedb",
-    creates => "/var/lib/mlocate/mlocate.db",
-    require => [
-      File["/etc/cron.daily/updatedb"],
-      Package['mlocate'],
-      Class['godwrap'],
-    ]
+  service { "updatedb.timer":
+    ensure => running,
+    enable => true,
+    require => Package['mlocate'],
   }
 }
