fa0f818356cec09d652f0198d307f3e1ae818f9c
Jesse Luehrs <doy@tozt.net>
2024-06-23 14:31:51 -0400

    go back to sftp backend for restic

--- modules/restic/manifests/remote.pp
+++ modules/restic/manifests/remote.pp
@@ -9,15 +9,9 @@ class restic::remote($host = 'partofme', $extra_paths = []) {
     content => template("restic/ssh");
   }
 
-  $rest_password = secret::value('restic-rest')
-
   class { 'restic::instance':
-    repo => "rest:http://partofme:8000/${facts['networking']['hostname']}",
+    repo => "sftp:restic@restic:/media/persistent/restic/${facts['networking']['hostname']}",
     extra_paths => $extra_paths,
-    extra_env => [
-      "RESTIC_REST_USERNAME=${facts['networking']['hostname']}",
-      "RESTIC_REST_PASSWORD=${rest_password}"
-    ],
     require => [
       File["/etc/ssh/restic"],
       File["/etc/ssh/ssh_config.d/restic.conf"],
