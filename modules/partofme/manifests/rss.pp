8c950d4e6913d4422e88eac570d6d48d237c3eb9
Jesse Luehrs <doy@tozt.net>
2025-12-03 22:21:13 -0500

    deps

--- modules/partofme/manifests/rss.pp
+++ modules/partofme/manifests/rss.pp
@@ -1,10 +1,8 @@
 class partofme::rss {
   include partofme::persistent
 
-  $data_dir = "/media/persistent/freshrss";
-
   class { "freshrss":
-    data_dir => "$data_dir",
-    require => Class["partofme::persistent"];
+    data_dir => "/media/persistent/freshrss",
+    require => File["/media/persistent"];
   }
 }
