da303a204804be1a01d75675971a59ad002a19da
Jesse Luehrs <doy@tozt.net>
2025-12-03 22:31:35 -0500

    actually the htaccess stuff needs to be with the docker image

--- modules/partofme/manifests/rss.pp
+++ modules/partofme/manifests/rss.pp
@@ -1,8 +1,29 @@
 class partofme::rss {
   include partofme::persistent
 
+  $data_dir = "/media/persistent/freshrss";
+
+  file {
+    "$data_dir":
+      ensure => directory;
+    "$data_dir/.htaccess":
+      source => 'puppet:///modules/partofme/freshrss-htaccess',
+      require => [
+        File["/media/persistent"],
+        File["$data_dir"],
+      ];
+  }
+
+  secret { "$data_dir/.htpasswd":
+    source => "rss",
+    require => [
+      File["/media/persistent"],
+      File["$data_dir"],
+    ];
+  }
+
   class { "freshrss":
-    data_dir => "/media/persistent/freshrss",
+    data_dir => "$data_dir",
     require => File["/media/persistent"];
   }
 }
--- modules/tozt/manifests/rss.pp
+++ modules/tozt/manifests/rss.pp
@@ -2,29 +2,6 @@ class tozt::rss {
   include tozt::certbot
   include tozt::persistent
 
-  $data_dir = "/media/persistent/freshrss";
-
-  file {
-    "$data_dir":
-      ensure => directory;
-    "$data_dir/.htaccess":
-      source => 'puppet:///modules/tozt/freshrss-htaccess',
-      require => [
-        Class["tozt::persistent"],
-        Package['nginx'],
-        File["$data_dir"],
-      ];
-  }
-
-  secret { "$data_dir/.htpasswd":
-    source => "rss",
-    require => [
-      Class["tozt::persistent"],
-      Package['nginx'],
-      File["$data_dir"],
-    ];
-  }
-
   nginx::site {
     "freshrss-tls":
       source => 'puppet:///modules/tozt/nginx/rss-tls.conf',
