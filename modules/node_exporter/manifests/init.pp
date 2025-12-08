cd33f8f7dc7e3d7dc43a221fbc0ee61e19114abd
Jesse Luehrs <doy@tozt.net>
2025-12-07 19:29:28 -0500

    enable the textfile collector

--- modules/node_exporter/manifests/init.pp
+++ modules/node_exporter/manifests/init.pp
@@ -3,9 +3,17 @@ class node_exporter {
     ensure => installed;
   }
 
+  file { "/etc/conf.d/prometheus-node-exporter":
+    source => "puppet:///modules/node_exporter/conf",
+    require => Package["prometheus-node-exporter"];
+  }
+
   service { "prometheus-node-exporter":
     ensure => running,
     enable => true,
-    subscribe => Package["prometheus-node-exporter"];
+    subscribe => [
+      Package["prometheus-node-exporter"],
+      File["/etc/conf.d/prometheus-node-exporter"],
+    ];
   }
 }
