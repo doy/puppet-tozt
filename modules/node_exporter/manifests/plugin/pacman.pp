cf7375bedf74f423fde1f1e2f6fed294e4b05641
Jesse Luehrs <doy@tozt.net>
2025-12-07 20:12:22 -0500

    fix typo

--- modules/node_exporter/manifests/plugin/pacman.pp
+++ modules/node_exporter/manifests/plugin/pacman.pp
@@ -1,4 +1,4 @@
-class node_exporter::plugins::pacman {
+class node_exporter::plugin::pacman {
   node_exporter::plugin { "pacman":
     source => "puppet:///modules/node_exporter/plugins/pacman",
     frequency => "hourly",
