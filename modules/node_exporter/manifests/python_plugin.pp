a6bd05e40e56287625ddd5672518bc696c30480a
Jesse Luehrs <doy@tozt.net>
2025-12-08 22:49:43 -0500

    make sure that python-cryptography is installed

--- modules/node_exporter/manifests/python_plugin.pp
+++ modules/node_exporter/manifests/python_plugin.pp
@@ -1,5 +1,9 @@
 class node_exporter::python_plugin {
-  package { "python-prometheus_client":
+  package {
+    [
+      "python-prometheus_client",
+      "python-cryptography",
+    ]:
     ensure => installed;
   }
 }
