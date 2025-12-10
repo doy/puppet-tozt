3a3b374233e8b333b4719348996916dc4ebb5efc
Jesse Luehrs <doy@tozt.net>
2025-12-09 21:54:18 -0500

    add smartmon plugin

---
+++ modules/node_exporter/manifests/plugin/smartmon.pp
@@ -0,0 +1,10 @@
+class node_exporter::plugin::smartmon {
+  include node_exporter::python_plugin
+
+  node_exporter::plugin { "smartmon":
+    source => "puppet:///modules/node_exporter/plugins/smartmon",
+    frequency => "hourly",
+    root => true,
+    require => Class['node_exporter::python_plugin'];
+  }
+}
