da59c1dd23a511ec074efc6534117c19627a4a26
Jesse Luehrs <doy@tozt.net>
2025-12-10 21:58:28 -0500

    fix

--- modules/node_exporter/manifests/plugin/smartmon.pp
+++ modules/node_exporter/manifests/plugin/smartmon.pp
@@ -10,7 +10,6 @@ class node_exporter::plugin::smartmon {
     source => "puppet:///modules/node_exporter/plugins/smartmon",
     frequency => "hourly",
     root => true,
-    needs_persist => true,
     require => Class['node_exporter::python_plugin'];
   }
 }
