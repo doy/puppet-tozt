896f65eca837c148ede1ac01a8d464d1ec04a5f2
Jesse Luehrs <doy@tozt.net>
2025-12-10 21:57:51 -0500

    fix

--- modules/node_exporter/manifests/plugin/rclone.pp
+++ modules/node_exporter/manifests/plugin/rclone.pp
@@ -9,7 +9,6 @@ class node_exporter::plugin::rclone {
   node_exporter::plugin { "rclone":
     source => "puppet:///modules/node_exporter/plugins/rclone",
     frequency => "00/6:00",
-    needs_network => true,
     require => Class['node_exporter::python_plugin'];
   }
 }
--- modules/node_exporter/manifests/plugin/restic.pp
+++ modules/node_exporter/manifests/plugin/restic.pp
@@ -15,9 +15,6 @@ class node_exporter::plugin::restic {
     source => "puppet:///modules/node_exporter/plugins/restic",
     frequency => "hourly",
     root => true,
-    needs_network => $_needs_network,
-    needs_persist => $_needs_persist,
-    after => $_after,
     require => Class['node_exporter::python_plugin'];
   }
 }
