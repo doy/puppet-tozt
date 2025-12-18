2ec626e1f6296689c3bfdcae64aeb29c46f84a35
Jesse Luehrs <doy@tozt.net>
2025-12-18 01:37:32 -0500

    also add some nginx rate limiting for cgit

--- modules/tozt/manifests/git.pp
+++ modules/tozt/manifests/git.pp
@@ -2,6 +2,7 @@ class tozt::git {
   include tozt::certbot
 
   nginx::vhost { "git":
-    content => file('tozt/nginx/git');
+    content => file('tozt/nginx/git'),
+    prefix => file('tozt/nginx/git-prefix');
   }
 }
